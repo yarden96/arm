23cb96b1fe1ae7a5a0830621d17b5209d525e814
Yarden Bitan <yarden11bitan@gmail.com>
2020-05-15 11:44:13 +0300

    all

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://requests-managemant.herokuapp.com/');
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization, X-Requested-With, X-HTTP-Method-Override, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -18,23 +17,15 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.on('ready', () => {
-//   mainWindow = new BrowserWindow({
-//       webPreferences: {
-//           nodeIntegration: true
-//       }
-//   });
-// });
-
 // Express Middleware for serving static files
 app.use(express.static(path.join(__dirname)));
 
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/webgl_loader_fbx.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 const port = process.env.PORT || 3001;
 app.listen(port, ()=> {
-    console.log('Server running');
+    console.log('Server running on port ', port);
 })
